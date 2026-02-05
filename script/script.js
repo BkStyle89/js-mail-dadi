cf8805e373421a6565e82b2a0f97dede6544875f
BkStyle89 <kajzer@hotmail.it>
2026-02-05 15:19:46 +0100

    invited list

--- script/script.js
+++ script/script.js
@@ -3,5 +3,18 @@
 //appropriate msg info
 //gioco di dadi
 
-const list = ['Marco','Antonio','Giuseppe','Francesco','Flavio','Mirko'];
-console.log(list);
+const list = ['Marco@gmail.com','Antonio@gmail.com','Giuseppe@gmail.com','Francesco@gmail.com','Flavio@gmail.com','Mirko@gmail.com'];
+/* console.log(list); */
+
+let mail = prompt('inserisci la tua mail')
+console.log(mail)
+
+let message = ('messaggio di invito')
+
+{if(list.includes(mail)) 
+    message = 'sei stato invitato'
+    else
+    message = 'non sei stato invitato'
+
+}
+console.log(message)
